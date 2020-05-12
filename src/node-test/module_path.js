016edcd60d318cb10422d742e721c858d5cf61f4
kexinweb <1657619661@qq.com>
2020-05-12 16:22:09 +0800

    WIP: node测试代码

--- src/node-test/module_path.js
+++ src/node-test/module_path.js
@@ -2,6 +2,7 @@
 // console.log(require.extensions)
 // console.log(a)
 
+console.log(require.cache)
 // 这一段用来测试exports不能直接赋值
 var change = function(a) {
     a = {
@@ -15,3 +16,4 @@ var a = {
 
 change(a) 
 console.log(a)
+
